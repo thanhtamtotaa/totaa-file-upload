e78861d07325d41d593f0b266536b24a46d471e7
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-18 16:08:49 +0700

    Create FileUpload.php

---
+++ src/Models/FileUpload.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Totaa\TotaaFileUpload\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Wildside\Userstamps\Userstamps;
+
+class FileUpload extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    use Userstamps;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'totaa_file_uploads';
+
+    // Disable Laravel's mass assignment protection
+    protected $guarded = ['id'];
+}
