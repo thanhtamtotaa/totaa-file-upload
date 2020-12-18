fc2db85aa719ed4584c21a609114bc2f3bcaf069
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-18 16:08:47 +0700

    Create 2020_12_18_155850_create_totaa_file_uploads_table.php

---
+++ database/migrations/2020_12_18_155850_create_totaa_file_uploads_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTotaaFileUploadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('totaa_file_uploads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('drive', 100);
+            $table->longText('local_path')->nullable()->default(null);
+            $table->longText('google_drive_path')->nullable()->default(null);
+            $table->longText('google_drive_id')->nullable()->default(null);
+            $table->boolean('active')->nullable()->default(true);
+            $table->unsignedBigInteger('created_by')->nullable()->default(null);
+            $table->unsignedBigInteger('updated_by')->nullable()->default(null);
+            $table->unsignedBigInteger('deleted_by')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('totaa_file_uploads');
+    }
+}
