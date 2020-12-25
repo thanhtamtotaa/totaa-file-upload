76be35fa5d1424ccb79ef0e2c6cdb09481ec63c4
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-25 17:19:31 +0700

    Update FileUpload.php

--- src/Models/FileUpload.php
+++ src/Models/FileUpload.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Wildside\Userstamps\Userstamps;
+use Illuminate\Support\Facades\Storage;
 
 class FileUpload extends Model
 {
@@ -22,4 +23,81 @@ class FileUpload extends Model
 
     // Disable Laravel's mass assignment protection
     protected $guarded = ['id'];
+
+    protected $disk, $adapter;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMimetype()
+    {
+        if ($this->drive == "public") {
+            $path = $this->local_path;
+        } else {
+            $path = $this->google_drive_display_path;
+        }
+
+        return $this->disk = Storage::disk($this->drive)->mimeType($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUrl()
+    {
+        if ($this->drive == "public") {
+            $path = $this->local_path;
+        } else {
+            $path = $this->google_drive_display_path;
+        }
+
+        return $this->disk = Storage::disk($this->drive)->url($path);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getThumbnail()
+    {
+        if ($this->drive == "public") {
+            return $this->disk = Storage::disk($this->drive)->url($this->local_path);
+        } else {
+            dd(Storage::disk($this->drive)->getDriver()->getAdapter()->getFileObject($this->google_drive_virtual_path));
+            return $this->disk = Storage::disk($this->drive)->url($this->google_drive_display_path);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGallery()
+    {
+        $gallery = [];
+
+        if ($this->drive == "public") {
+            $gallery['mimetype'] = Storage::disk($this->drive)->mimeType($this->local_path);
+            $gallery['url'] = Storage::disk($this->drive)->url($this->local_path);
+            $gallery['thumbnail'] = Storage::disk($this->drive)->url($this->local_path);
+        } else {
+            $FileObject = Storage::disk($this->drive)->getDriver()->getAdapter()->getFileObject($this->google_drive_virtual_path);
+
+            $gallery['mimetype'] = $FileObject->getMimetype();
+
+            if(($url = $FileObject->getWebContentLink())) {
+                $gallery['url'] = str_replace('export=download', 'export=media', $url);
+            } elseif (($url = $FileObject->getWebViewLink())) {
+                $gallery['url'] = $url;
+            }
+
+            if(($thumbnail = $FileObject->getThumbnailLink())) {
+                $gallery['thumbnail'] = $thumbnail;
+            } elseif (($thumbnail = $FileObject->getWebContentLink())) {
+                $gallery['thumbnail'] = str_replace('export=download', 'export=media', $thumbnail);
+            } elseif (($thumbnail = $FileObject->getWebViewLink())) {
+                $gallery['thumbnail'] = $thumbnail;
+            }
+        }
+
+        return $gallery;
+    }
 }
